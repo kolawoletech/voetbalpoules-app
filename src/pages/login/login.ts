103697e43fc891371614b873914d7a3135d64e2f
Jeroen Klarenbeek <info@voetbalpoules.nl>
2018-02-05 21:17:49 +0100

    bugfixjes, login scherm met next button in keyboard

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController, Platform } from 'ionic-angular';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 import { TabsNavigationPage } from '../tabs-navigation/tabs-navigation';
@@ -9,6 +9,8 @@ import { FacebookLoginService } from '../../providers/facebook/facebook-login.se
 import { AuthService } from '../../providers/auth/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs/Observable';
+import { Keyboard } from '@ionic-native/keyboard';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @Component({
   selector: 'login-page',
@@ -26,7 +28,10 @@ export class LoginPage {
     public loadingCtrl: LoadingController,
     public authService: AuthService,
     private alertController: AlertController,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private platform: Platform,
+    private keyboard: Keyboard,
+    private statusBar: StatusBar
   ) 
   {
     this.main_page = { component: TabsNavigationPage };
@@ -39,6 +44,14 @@ export class LoginPage {
     this.login = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('test', Validators.required)
+    });
+  }
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.keyboard.hideKeyboardAccessoryBar(false);
     });
   }
 
@@ -76,7 +89,7 @@ export class LoginPage {
   goToSignup() {
     Observable.forkJoin(
       this.translate.get('NOT_IMPLEMENTED'),
-      this.translate.get('NOT_IMPLEMENTED_SIGNUP'),
+      this.translate.get('NOT_IMPLEMENTED_SIGN_UP'),
       this.translate.get('OK')
     ).subscribe(data => {
       let alert = this.alertController.create({
