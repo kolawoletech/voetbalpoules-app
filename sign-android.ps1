c316259d0661cf98e3126972985b1f018a21a4e2
Jeroen Klarenbeek <info@voetbalpoules.nl>
2018-03-11 16:04:44 +0100

    bugfix met internet permissie

--- sign-android.ps1
+++ sign-android.ps1
@@ -1,7 +1,10 @@
 keytool -importkeystore -srckeystore voetbalpoules-android.jks -destkeystore voetbalpoules-android.jks -deststoretype pkcs12
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore voetbalpoules-android.jks C:\github\voetbalpoules-app\platforms\android\build\outputs\apk\release\android-release-unsigned.apk voetbalpoules-android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ..\voetbalpoules-android.jks C:\github\voetbalpoules-app\platforms\android\build\outputs\apk\release\android-release-unsigned.apk voetbalpoules-android
 
-%APPDATA$\Local\Android\Sdk\build-tools\27.0.3\zipalign.exe -v 4 C:\github\voetbalpoules-app\platforms\android\build\outputs\apk\release\android-release-unsigned.apk .\voetbalpoules.apk
+%LOCALAPPDATA%\Android\Sdk\build-tools\27.0.3\
 
-%APPDATA%\Local\Android\Sdk\build-tools\27.0.3\apksigner verify .\voetbalpoules.apk
+del ..\voetbalpoules.apk
+zipalign.exe -v 4 .\platforms\android\build\outputs\apk\release\android-release-unsigned.apk ..\voetbalpoules.apk
+
+apksigner verify ..\voetbalpoules.apk
