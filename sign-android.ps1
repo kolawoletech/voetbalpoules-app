6419c37b4bbd7803dd749caf6ba02c9581945f92
Jeroen Klarenbeek <info@voetbalpoules.nl>
2018-08-26 11:55:17 +0200

    update Android SDK

--- sign-android.ps1
+++ sign-android.ps1
@@ -1,11 +1,11 @@
 keytool -importkeystore -srckeystore voetbalpoules-android.jks -destkeystore voetbalpoules-android.jks -deststoretype pkcs12
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ..\voetbalpoules-android.jks C:\github\voetbalpoules-app\platforms\android\build\outputs\apk\release\android-release-unsigned.apk voetbalpoules-android
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ..\voetbalpoules-android.jks C:\github\voetbalpoules-app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk voetbalpoules-android
 
 %LOCALAPPDATA%\Android\Sdk\build-tools\27.0.3\
 
 del ..\voetbalpoules.apk
-zipalign.exe -v 4 .\platforms\android\build\outputs\apk\release\android-release-unsigned.apk ..\voetbalpoules.apk
+zipalign.exe -v 4 .\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk ..\voetbalpoules.apk
 
 apksigner verify ..\voetbalpoules.apk
 
