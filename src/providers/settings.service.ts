8db121c9e5af6eaa78039ede6af832ab1e51e848
Jeroen Klarenbeek <info@voetbalpoules.nl>
2018-10-30 00:40:25 +0100

    oei, verkeerde endpoints

--- src/providers/settings.service.ts
+++ src/providers/settings.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SettingsService {
-    public AuthorizationApiEndpoint: string = 'https://auth.voetbalpoules.nl';
-    public PouleApiEndpoint: string = 'https://api.voetbalpoules.nl';
+    public AuthorizationApiEndpoint: string = 'https://localhost:44366';
+    public PouleApiEndpoint: string = 'https://localhost:44396';
     public LogoEndpoint: string = 'https://localhost:44300/foto';
 
     constructor(private isDebug: IsDebug) { 
         this.isDebug.getIsDebug()
             .then((isDebug: boolean) => {
-                if(isDebug) {
+                if(!isDebug) {
                     this.AuthorizationApiEndpoint = 'https://auth.voetbalpoules.nl';
                     this.PouleApiEndpoint = 'https://api.voetbalpoules.nl';
                     this.LogoEndpoint = 'https://vp-logos.azureedge.net';
